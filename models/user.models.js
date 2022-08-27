78b13a7e79cccdd8b1625dd36e2b56747b95cdb3
mipunaruwan <nipunakalpa1234@gmail.com>
2022-08-28 00:34:36 +0530

    usermodel completed

---
+++ models/user.models.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose')
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    gender : {
+        type : String,
+        required : true
+    },
+
+    dob : {
+        type : String,
+        required : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    },
+
+    phoneNo : {
+        type : Number,
+        required : true
+    },
+
+    email : {
+        type : String,
+        required : true
+    }
+})
+module.exports=mongoose.model('user',userSchema)
